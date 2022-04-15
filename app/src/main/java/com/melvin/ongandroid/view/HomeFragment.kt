6881e0da571557ed85904ebbd9002770dbd79db4
cavigna <40524472+cavigna@users.noreply.github.com>
2022-04-15 13:17:23 -0400

    Added logic for changing AppBarTitle

--- app/src/main/java/com/melvin/ongandroid/view/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.FragmentHomeBinding
 
@@ -19,6 +20,11 @@ class HomeFragment : Fragment() {
     ): View {
 
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        actionBar?.title = getString(R.string.inicio)
+
+        binding.textView.text = getString(R.string.inicio)
 
         return binding.root
     }
