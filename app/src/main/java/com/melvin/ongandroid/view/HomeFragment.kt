0c32faa1ff4553b4d86e340f4d4d1a9f85c78c76
cavigna <40524472+cavigna@users.noreply.github.com>
2022-04-15 12:32:09 -0400

    Added HomeFragment.kt + ViewBinding

---
+++ app/src/main/java/com/melvin/ongandroid/view/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.melvin.ongandroid.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+
+}
