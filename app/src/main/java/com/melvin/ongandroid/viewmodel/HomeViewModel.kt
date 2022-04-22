d4ec753cf67bee1c5630ae66808e58ce8ff8c025
cavigna <40524472+cavigna@users.noreply.github.com>
2022-04-22 18:13:38 -0400

    Added HomeViewModel.kt

---
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.melvin.ongandroid.viewmodel
+
+import com.melvin.ongandroid.repository.OngRepository
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor(private val repo: OngRepository) {
+}
