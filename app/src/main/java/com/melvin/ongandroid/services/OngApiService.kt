bdf4ecbf58d13325d45fba4ecaaf10ba15b8c7bc
cavigna <40524472+cavigna@users.noreply.github.com>
2022-04-22 18:07:36 -0400

    Added api request to fetch latestNews

--- app/src/main/java/com/melvin/ongandroid/services/OngApiService.kt
+++ app/src/main/java/com/melvin/ongandroid/services/OngApiService.kt
@@ -1,4 +1,10 @@
 package com.melvin.ongandroid.services
 
+import com.melvin.ongandroid.model.NewsResponse
+import retrofit2.http.GET
+
 interface OngApiService {
+
+    @GET("news")
+    suspend fun fetchLatestNews() : NewsResponse
 }
