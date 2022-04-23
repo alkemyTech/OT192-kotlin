e3e2183f9f31b20e120b6481ad4181ed668a024c
cavigna <40524472+cavigna@users.noreply.github.com>
2022-04-23 12:26:00 -0400

    Added Logic in ContactViewModel.kt to enable/disable Button

--- app/src/main/java/com/melvin/ongandroid/viewmodel/ContactViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/ContactViewModel.kt
@@ -1,5 +1,7 @@
 package com.melvin.ongandroid.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.melvin.ongandroid.repository.OngRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -7,5 +9,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ContactViewModel @Inject constructor(repo: OngRepository) : ViewModel() {
-    // Implement the ViewModel
+
+    // LiveData to disable/enable Button on ContactForm when is correct
+    private val _isButtonEnabled = MutableLiveData<Boolean>(false)
+    val isButtonEneabled: LiveData<Boolean> = _isButtonEnabled
+
+    // Parameters from the Contact Form. So we can observe changes in ViewModel
+    val firstName  = MutableLiveData<String>("")
+    val lastName  = MutableLiveData<String>("")
+    val email  = MutableLiveData<String>("")
+    val conctactMessage  = MutableLiveData<String>("")
 }
