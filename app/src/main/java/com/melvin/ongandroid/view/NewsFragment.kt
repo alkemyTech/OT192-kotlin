c8d991be2a8fc20fc816aa13df059d420261a291
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-04-30 13:59:31 -0300

    Borre imports innecesarios y arreglé un bug en la navegacion del boton ver mas de novedades

--- app/src/main/java/com/melvin/ongandroid/view/NewsFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/NewsFragment.kt
@@ -7,8 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.melvin.ongandroid.databinding.FragmentNewsBinding
 import com.melvin.ongandroid.model.News
 import com.melvin.ongandroid.view.adapters.NewsItemAdapter
