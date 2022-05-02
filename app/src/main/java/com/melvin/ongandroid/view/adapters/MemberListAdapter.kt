225690700bcc9693e2f5873cf7b2aa02df2596b2
Ignacio Cavallo <40524472+cavigna@users.noreply.github.com>
2022-05-02 14:47:20 -0400

    Ot192 46 integracion members (#43)
    * Added Model class Members.kt
    
    * Added fetchMembers in OngApiService.kt that returns a Response of type Generic Response with a List Of Members
    
    * Reformatted code according to kotlin conventions.
    
    * Added fetch Members in OngRepository.kt. Handles states of endpoint response with success, error form api and exceptions.
    
    * Refactored fetchMembers
    
    * Added function that fetchs members from OngRepository.kt. Handle States and post its value on a MutableLive Data
    
    * Created MemberListAdapter.kt to replace MemberItemAdapter.kt.
    
    * Added function that handles States of Members Response. If success, shows section and hides progress spinner. If Loading, hides section and shows spinner

---
+++ app/src/main/java/com/melvin/ongandroid/view/adapters/MemberListAdapter.kt
@@ -0,0 +1,101 @@
+package com.melvin.ongandroid.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.melvin.ongandroid.databinding.ItemRecyclerAboutUsBinding
+import com.melvin.ongandroid.model.MemberUI
+import com.melvin.ongandroid.model.Members
+
+/**
+ * Class for ListAdapter that replaces [MemberItemAdapter].
+ * Now uses [Members] model from api response. Replaces
+ * [MemberUI] used to mock responseC
+ */
+class MemberListAdapter() :
+    ListAdapter<Members, RecyclerView.ViewHolder>
+        (DiffUtilCallback()) {
+
+    var onItemClicked: ((Members) -> Unit)? = null
+
+    /**
+     * On create view holder
+     *
+     * @param parent
+     * @param viewType
+     * @return constructor of ViewHolder referencing inflated layout
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = ItemRecyclerAboutUsBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    /**
+     * On bind view holder
+     * Connect the item in the position of the list with the Holder
+     *
+     * @param holder
+     * @param position of the item in the list
+     */
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewHolder: ViewHolder = holder as ViewHolder
+        viewHolder.bind(getItem(position), onItemClicked)
+    }
+
+    /**
+     * View holder
+     *
+     * @property binding reference to item layout
+     * @constructor Create empty View holder
+     */
+    class ViewHolder(private val binding: ItemRecyclerAboutUsBinding) :
+        RecyclerView.ViewHolder(binding.cvAboutUs) {
+
+        /**
+         * Bind
+         * Used to connect items to layout
+         *
+         * @param value the item
+         * @param listener a function to invoke when the item in clicked
+         */
+        internal fun bind(
+            value: Members,
+            listener: ((Members) -> Unit)?,
+        ) {
+            with(binding) {
+                //Set texts
+                tvName.text = value.name
+                tvDescription.text = value.description
+                //Load the image url and set it on this ImageView
+                imgMember.load(value.image)
+
+                cvAboutUs.setOnClickListener {
+                    listener?.invoke(value)
+                }
+            }
+        }
+    }
+
+    /**
+     * Diff util callback
+     *
+     * @constructor Create empty Diff util callback
+     */
+    class DiffUtilCallback : DiffUtil.ItemCallback<Members>(){
+        override fun areItemsTheSame(oldItem: Members, newItem: Members): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Members, newItem: Members): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+}
