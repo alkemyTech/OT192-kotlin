d625b9e8f368aae97d95411041f91fe01f9badac
cavigna <40524472+cavigna@users.noreply.github.com>
2022-05-05 16:31:28 -0400

    Added Function for navigation to SignUpFragment.kt

--- app/src/main/java/com/melvin/ongandroid/view/login/LogInFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/login/LogInFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.FragmentLogInBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,7 +24,19 @@ class LogInFragment : Fragment() {
 
         binding = FragmentLogInBinding.inflate(layoutInflater, container, false)
 
+
+        irASignUp()
         return binding.root
+    }
+
+    /**
+     * Navigates to [R.id.action_logInFragment_to_SignUpFragment]
+     * when user clicks on button SignUp
+     */
+    private fun irASignUp() {
+        binding.buttonSignUpLogin.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_logInFragment_to_SignUpFragment)
+        }
     }
 
 
