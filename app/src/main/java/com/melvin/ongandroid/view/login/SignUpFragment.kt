6886c1e76594eac8eea63dc22bfb1f86042e930e
Jonathan Sergio Rodriguez Alvarez <53452576+jonathanr123@users.noreply.github.com>
2022-05-05 17:08:17 -0300

    Diseño del layout de Sign Up. (#54)
    * Diseño del layout de Sign Up.
    
    * Modifique el nav_graph_login incorporando vista Sign Up.

---
+++ app/src/main/java/com/melvin/ongandroid/view/login/SignUpFragment.kt
@@ -0,0 +1,24 @@
+package com.melvin.ongandroid.view.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.melvin.ongandroid.databinding.FragmentSignUpBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSignUpBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
