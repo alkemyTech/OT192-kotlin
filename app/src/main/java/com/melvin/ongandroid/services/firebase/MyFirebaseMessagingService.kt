ce4f25eacd8ade5b41859fc658e71c5bec1c06ab
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-05-05 18:36:04 -0300

    Configuración de las notificaciones de Firebase Cloud Messaging.

---
+++ app/src/main/java/com/melvin/ongandroid/services/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,10 @@
+package com.melvin.ongandroid.services.firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Created by Jonathan on 05/05/2022.
+ */
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+}
