9ee73d6b200d52b75e1a05c924cfefd6adaa4a63
leonelgomez1990 <leonelgomez@live.com.ar>
2022-05-16 20:43:58 -0300

    Implemented Timer Delay and toast message in Splash
    1) Delay of five seconds before going to next screen
    2) Show a toast message when timer has finished

--- app/src/main/java/com/melvin/ongandroid/view/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/SplashActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.ActivitySplashBinding
 import com.melvin.ongandroid.viewmodel.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,10 +33,21 @@ class SplashActivity : AppCompatActivity() {
 
         // Coroutine to start Activity specified and finish SplashActivity
         CoroutineScope(Dispatchers.Main).launch {
+
+            // delay of five seconds before going to next screen - 2022-05-16 L.Gomez
+            delay(5000)
+            // Show a toast message when timer has finished - 2022-05-16 L.Gomez
+            showWelcomeMessage()
+
             setObservers()
             finish()
         }
 
+    }
+
+    // Show a toast with a welcome message - 2022-05-16 L.Gomez
+    private fun showWelcomeMessage() {
+        Toast.makeText(this, getString(R.string.splash_timer_message), Toast.LENGTH_LONG).show()
     }
 
     // Set observers for view model
